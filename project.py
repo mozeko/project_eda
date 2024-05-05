de5e37f74cb9ea1f655370b7f7c8268006f2e7c9
Zakaria Mostafa <149090419+mozeko@users.noreply.github.com>
2024-05-05 15:14:34 +0400

    Update project.py

--- project.py
+++ project.py
@@ -21,7 +21,7 @@ if selected == "Home":
     st.title("Flight Fare Price Prediction App ✈")
     st.image("fl.jpg")
     st.write("Airlines play a pivotal role in global transportation, connecting people and goods across vast distances. They operate a complex network of routes, managing everything from aircraft maintenance to customer service Airlines own or lease a fleet of aircraft to transport passengers and cargo. These fleets can range from small regional jets to large intercontinental planes. Airlines factor in various operating costs, including fuel, aircraft maintenance, crew salaries, airport fees, and insurance. Ticket prices fluctuate based on supply and demand dynamics. Popular routes during peak travel seasons often have higher prices. Prices are influenced by competition within the airline industry. Airlines may adjust their fares to remain competitive with other carriers operating on the same routes Airlines generate additional revenue through ancillary services like baggage fees, seat selection, in-flight meals, and entertainment In essence, airline ticket prices are the result of a complex interplay between operational costs, market forces, and consumer behavior. By understanding these factors, travelers can make informed decisions when booking flights.")
-    st.sidebar.markdown("Mad With :heart_eyes: by Analyst.[Zakaria Mostafa](https://www.linkedin.com/in/zakariamostafa/) ")
+    st.sidebar.markdown("Made With :heart_eyes: by Analyst.[Zakaria Mostafa](https://www.linkedin.com/in/zakariamostafa/) ")
     
 if selected =="EDA":
     # loadind data
